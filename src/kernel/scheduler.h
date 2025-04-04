4a85b66ab1615f8c6626241ecd49f1f7ecdfb981
Krystof Purtell <128022752+KrystofPurtell@users.noreply.github.com>
2025-04-04 17:05:15 -0400

    Scheduler is much more built out, priority queues are also more built out.

---
+++ src/kernel/scheduler.h
@@ -0,0 +1,61 @@
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+
+#include "lib/Vec.h"
+#include "kern-pcb.h"
+#include "lib/spthread.h"
+#include <stdbool.h> 
+
+/**
+ * @brief Initializes the scheduler queues. This function should be called
+ *        before any other scheduler functions are called.
+ */
+void initialize_scheduler_queues();
+
+/**
+ * @brief Frees the scheduler queues. This function should be called when
+ *        the scheduler is no longer needed.
+ */
+void free_scheduler_queues();
+
+/**
+ * @brief Chooses a random integer from 0, 1, 2 at the prescribed 
+ *        probabilites. In particular, 0 is output 1.5x more than
+ *        1, which is output 1.5x more than 2. Notably, it accounts
+ *        for cases where some of the queues are empty. As a result,
+ *        all output integers are guaranteed to be references to
+ *        non-empty queues. This function should never be called when
+ *        all queues are empty consequently.
+ * 
+ * @return int 0, 1, or 2
+ */
+int generate_next_priority();
+
+/**
+ * @brief Returns the next PCB in the queue of the specified priority.
+ *        Assumes that the queue is not empty and consequently should 
+ *        never be called when the queue is empty. Notably, it removes 
+ *        the PCB from the queue. 
+ * 
+ * @param priority queue priority to get next PCB from
+ * @return         a ptr to the next pcb struct in queue
+ */
+pcb_t* get_next_pcb(int priority);
+
+
+/**
+ * @brief Puts the given pcb struct pointer into its appropriate
+ *        queue. Notably, it solely uses the pcb's interal fields
+ *        to determine the correct queue (priority and state).
+ */
+void put_pcb_into_correct_queue(pcb_t* pcb);
+
+/**
+ * @brief TODO
+ */
+void scheduler();
+
+
+
+
+#endif // SCHEDULER_H_
--- src/pennos.c
+++ src/pennos.c
@@ -1,4 +1,18 @@
+#include "src/lib/Vec.h"
+#include "src/kernel/kern-pcb.h"
+#include "src/kernel/scheduler.h"
+#include <stdbool.h>
+
 
 int main() {
-    return 0;
+
+    // initialize scheduler architecture and init process
+    initialize_scheduler_queues();
+    // TODO --> initialzie init process
+
+    scheduler();
+
+    // cleanup
+    free_scheduler_queues();
+    // TODO --> add anything else that's needed
 }
