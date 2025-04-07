c449de56f495b641817cc4a447d1b0e721f180eb
Dan <dankim1@seas.upenn.edu>
2025-04-07 00:12:48 -0400

    add comments to fat.h

--- src/fs/fat.h
+++ src/fs/fat.h
@@ -4,20 +4,155 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+////////////////////////////////////////////////////////////////////////////////
+//                          PENNFAT DEFINITIONS                               //
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Special FAT entry value indicating the end of a file chain.
+ * 
+ * When this value is encountered in the FAT table, it signifies that
+ * the current block is the last block of a file.
+ */
 #define FAT_EOF 0xFFFF
+
+/**
+ * @brief Special FAT entry value indicating a free block.
+ * 
+ * When this value is found in the FAT table, it indicates that
+ * the corresponding block is not allocated to any file and is available
+ * for allocation.
+ */
 #define FAT_FREE 0x0000
 
-// FAT block management
-int fat_init(const char *fs_name);
+////////////////////////////////////////////////////////////////////////////////
+//                          PENNFAT FUNCTIONS                                 //
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Initialize a new FAT file system or format an existing one.
+ * 
+ * This function creates a new FAT file system on the specified file.
+ * If the file already exists, it will be formatted (all data will be lost).
+ * If the file does not exist, it will be created with the default size.
+ *
+ * @param fs_name The name of the file to use as the file system.
+ * @param num_blocks The total number of blocks in the file system.
+ * @param block_size The size of a block in bytes.
+ * @return On success, returns 0.
+ *         On error, returns -1 and sets P_ERRNO to indicate the error.
+ */
+int fat_init(const char *fs_name, uint16_t num_blocks, uint32_t block_size);
+
+/**
+ * @brief Load an existing FAT file system.
+ * 
+ * This function mounts an existing FAT file system from the specified file.
+ * It reads the superblock and FAT data into memory for fast access.
+ *
+ * @param fs_name The name of the file containing the file system.
+ * @return On success, returns 0.
+ *         On error, returns -1 and sets P_ERRNO to indicate the error.
+ */
+ int fat_load(const char *fs_name);
+
+/**
+ * @brief Unmount the currently mounted FAT file system.
+ * 
+ * This function synchronizes any cached data to disk and releases
+ * all resources associated with the file system. After calling this
+ * function, no further file system operations should be performed
+ * until fat_init() or fat_load() is called again.
+ */
 void fat_unmount();
+
+/**
+ * @brief Synchronize the FAT table to disk.
+ * 
+ * This function ensures that any in-memory changes to the FAT table
+ * are written to persistent storage.
+ *
+ * @return On success, returns 0.
+ *         On error, returns -1 and sets P_ERRNO to indicate the error.
+ */
+ int fat_save();
+
+/**
+ * @brief Get the value of a specific entry in the FAT table.
+ * 
+ * This function retrieves the FAT entry for the specified block index.
+ * This value indicates either the next block in the chain, FAT_EOF to
+ * indicate end of file, or FAT_FREE to indicate the block is not allocated.
+ *
+ * @param index The block index to look up in the FAT.
+ * @return The value stored in the FAT for the specified block.
+ *         Returns FAT_EOF if the index is invalid or the file system is not mounted.
+ */
 uint16_t fat_get_block(uint16_t index);
+
+/**
+ * @brief Set the value of a specific entry in the FAT table.
+ * 
+ * This function updates the FAT entry for the specified block index.
+ *
+ * @param index The block index to update in the FAT.
+ * @param value The new value to store (next block index, FAT_EOF, or FAT_FREE).
+ */
 void fat_set_block(uint16_t index, uint16_t value);
+
+/**
+ * @brief Find a free block in the FAT.
+ * 
+ * This function scans the FAT table to find a block that is marked as free
+ * (ie. FAT_FREE) and can be allocated for file storage.
+ *
+ * @return On success, returns the index of a free block.
+ *         If no free blocks are available or the file system is not mounted,
+ *         returns -1.
+ */
 int fat_find_free_block();
-int fat_save();
-int fat_load(const char *fs_name);
+
+/**
+ * @brief Get the block size used by the file system.
+ * 
+ * This function returns the size in bytes of each block in the FAT file system.
+ * All file operations work with this block size as the basic unit of allocation.
+ *
+ * @return The block size in bytes.
+ */
 int fat_get_block_size();
+
+/**
+ * @brief Get the offset to the data region.
+ * 
+ * This function returns the byte offset from the beginning of the file system
+ * to the start of the data region where file contents are stored.
+ *
+ * @return On success, returns the byte offset to the data region.
+ *         If the file system is not mounted, returns -1.
+ */
 int fat_get_data_offset();
+
+/**
+ * @brief Get the total number of entries in the FAT.
+ * 
+ * This function returns the total number of entries managed by the FAT,
+ * including both allocated and free blocks.
+ *
+ * @return On success, returns the number of entries in the FAT.
+ *         If the file system is not mounted, returns -1.
+ */
 int fat_get_num_entries();
+
+/**
+ * @brief Get the size of the FAT table in bytes.
+ * 
+ * This function returns the total size in bytes of the FAT table in memory.
+ * This is useful for memory management and debugging purposes.
+ *
+ * @return On success, returns the size of the FAT in bytes.
+ *         If the file system is not mounted, returns -1.
+ */
 int fat_get_fat_size_bytes();
 
 #endif
--- src/fs/fs_kfuncs.c
+++ src/fs/fs_kfuncs.c
@@ -1,11 +1,55 @@
-// k_open, k_read, k_write, k_close, k_unlink, k_lseek, k_ls
+#include "fs_kfuncs.h"
+#include "fat.h"
+#include "../lib/pennos-errno.h"
+#include "../kernel/kern-pcb.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h> // verify if needed
 
-#include "fs_syscalls.h"
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
 
-// TODO: Implement open with mode (F_READ, F_WRITE, F_APPEND)
-// TODO: Implement reading file by traversing FAT
-// TODO: Implement block-wise writing, handling allocation
-// TODO: Implement closing and releasing descriptors
-// TODO: Implement deletion with reference count check
-// TODO: Implement file seeking logic and offset update
-// TODO: Implement ls functionality to list file metadata
+// this struct is an entry in the global file descriptor table
+typedef struct {
+    bool in_use;
+    int flags;              // mode (F_READ, F_WRITE, F_APPEND)
+    int ref_count;          // tracks how many processes reference this file
+    uint32_t position;      // position = (block_index × block_size) + block_offset
+    int meta_idx;           // stores the index in the file system's metadata array 
+                            // where information about this file is stored (ie. name, size, firstBlock, etc.)
+    uint16_t curr_block;    // current block number corresponding to current file position
+    uint32_t block_offset;  // offset within current block
+} sys_fd_entry_t;
+
+// Global file descriptor table
+#define MAX_GLOBAL_FD 128 // TODO: check if this is the right size
+static sys_fd_entry_t sys_fd_table[MAX_GLOBAL_FD]; // TODO: check, do we want to make this static?
+
+int k_open(const char *fname, int mode) {
+    return -1; // TODO --> implement k_open
+}
+
+int k_read(int fd, int n, char *buf) {
+    return -1; // TODO --> implement k_read
+}
+
+int k_write(int fd, const char *str, int n) {
+    return -1; // TODO --> implement k_write
+}
+
+int k_close(int fd) {
+    return -1; // TODO --> implement k_close
+}
+
+int k_unlink(const char *fname) {
+    return -1; // TODO --> implement k_unlink
+}
+
+int k_lseek(int fd, int offset, int whence) {
+    return -1; // TODO --> implement k_lseek
+}
+
+int k_ls(const char *filename) {
+    return -1; // TODO --> implement k_ls
+}
