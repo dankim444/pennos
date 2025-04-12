97573b23b3e593c4b314b626b686ada24bff035c
Richard Zhang <richz@seas.upenn.edu>
2025-04-12 19:19:18 -0400

    waitpid and signal handling cleaned up, demo 0 working

--- src/kernel/signal.c
+++ src/kernel/signal.c
@@ -1,4 +1,5 @@
 #include <signal.h>
 
 // P_SIGSTOP, P_SIGCONT, P_SIGTERM handling
-// support for macros P_WIFEXITED, P_WIFSTOPPED, P_WIFSIGNALED for process statuses
+// support for macros P_WIFEXITED, P_WIFSTOPPED, P_WIFSIGNALED for process
+// statuses
